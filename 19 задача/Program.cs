b750d1235f0019f2fb3728cfb19f00e339089ce5
Aleksey Boyarkin <alekseyretouch@yandex.ru>
2022-09-22 20:18:36 +0300

    final

--- 19 задача/Program.cs
+++ 19 задача/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Задача 19
+    Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+    14212 -> нет
+    12821 -> да
+    23432 -> да
+*/
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+string A = Convert.ToString(number);
+
+if (A.Length == 5)
+{
+    if (A[0] == A[4]&& A[1] == A[3])
+    {
+        Console.WriteLine($"{number} является палиндромом");
+    }
+
+    else
+    {
+        Console.WriteLine($"{number} НЕ является палиндромом");
+    }
+}
+
+    else //( A.Length < 5 || A.Length > 5 ) 
+{
+    Console.WriteLine($"{number} НЕ пятизначное число");
+}
--- 23 задача/Program.cs
+++ 23 задача/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+    3 -> 1, 8, 27
+    5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int CurrentNumber = 1;
+int B = 0;
+
+
+while (CurrentNumber <= N)
+{
+    B = CurrentNumber * CurrentNumber * CurrentNumber; 
+    //number * number;
+    Console.Write($"{B}, ");
+    CurrentNumber++;
+}
+Console.WriteLine();
